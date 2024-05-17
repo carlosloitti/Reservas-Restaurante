ba4663344375a487ade74e632b29ea8f3f7cb953
Carlos Loitti <carlosloitir@gmail.com>
2024-05-17 14:29:17 -0500

    Tabla reservas + migracion

---
+++ database/migrations/2024_05_17_192853_create_reservas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cliente_id');
+            $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('cascade');
+            $table->unsignedBigInteger('mesa_id');
+            $table->foreign('mesa_id')->references('id')->on('mesas')->onDelete('cascade');
+            $table->dateTime('fecha_reserva');
+            $table->integer('duracion');
+            $table->enum('estado', ['pendiente', 'confirmada', 'cancelada'])->default('pendiente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservas');
+    }
+};
